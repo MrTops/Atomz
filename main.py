5b6e3c1c4fd69e907e9b89657cbaf2b046788f8d
MrTops <eldon.williams@stu.grayson.kyschools.us>
2020-02-01 01:40:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+message = """             ___  ___   ___                           ___   ___ 
+  .'|=|`.   `._|=|   |=|_.'   .'|=|`.     .'|\/|`.   |   |=|_.' 
+.'  | |  `.      |   |      .'  | |  `. .'  |  |  `. `.  |      
+|   |=|   |      |   |      |   | |   | |   |  |   |   `.|=|`.  
+|   | |   |      `.  |      `.  | |  .' |   |  |   |  ___  |  `.
+|___| |___|        `.|        `.|=|.'   |___|  |___|  `._|=|___|
+are the hip, new and cool way to organize your data.
+the main.py contains more information on how to use atoms and a example.
+documentation on the github.
+
+Key Points:
+1-Uses only prepackaged python dependencies!
+2-Scalable
+3-Easy to use
+4-File system-esk architecture
+"""
+
+print(message)
